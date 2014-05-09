ee3e53e2f91200b51fb5042a4955eb26be0ab255
DaanHaaz <danielhaazen@hotmail.com>
2014-05-09 21:42:44 +0200

    My commit messages suck

--- src/graphics.js
+++ src/graphics.js
@@ -2,11 +2,34 @@
 
 love.graphics = {};
 
-love.graphics.color = {r:"FF",g:"FF",b:"FF"};
+love.graphics.images = {};
 
-love.graphics.backgroundColor = {r:"00",g:"00",b:"00"};
+love.graphics.color = {r:255,g:255,b:255,a:255};
+
+love.graphics.backgroundColor = {r:0,g:0,b:0};
 
 love.graphics.pointSize = 1;
+
+love.graphics.fontSize = 10;
+
+love.graphics.defaultFilter = "linear";
+
+love.graphics.font = {};
+
+love.graphics.preload = function (a) {
+	for (var i = 0; i < arguments.length; i++) {
+		var name = arguments[i];
+		var img;
+		img = new Image();
+		img.src = name;
+		img.onload = function(){
+			love._assetsLoaded++;
+		}
+		this.images[name] = img;
+		love._assetsToBeLoaded++;
+		
+	};
+}
 
 
 //Drawing functions
@@ -28,6 +51,12 @@ love.graphics.arc = function (mode,x,y,r,a1,a2) {
 	this.ctx.arc(x,y,Math.abs(r),a1,a2);
 	this.ctx.lineTo(x,y);
 	love.graphics.mode(mode);
+}
+
+love.graphics.clear = function () {
+	this.ctx.fillStyle = this.rgb(this.backgroundColor.r,this.backgroundColor.b,this.backgroundColor.g);
+	this.background();
+	this.ctx.fillStyle = this.rgb(this.color.r,this.color.b,this.color.g);
 }
 
 love.graphics.line = function () {
@@ -68,6 +97,7 @@ love.graphics.polygon = function (mode, verts) {
 }
 
 love.graphics.print = function (t,x,y,r,sx,sy,ox,oy) {
+
 	x = x || 0;
 	y = y || 0;
 	r = r || 0;
@@ -83,88 +113,286 @@ love.graphics.print = function (t,x,y,r,sx,sy,ox,oy) {
 	this.ctx.restore();
 }
 
-love.graphics._draw = function (img,x,y,r,sx,sy,ox,oy) {
+//Note: Doesn't work perfect yet with rotation.
+love.graphics.printf = function (t,x,y,limit,align,r,sx,sy,ox,oy) {
+	x = x || 0;
+	y = y || 0;
+	r = r || 0;
 	sx = sx || 1;
 	sy = sy || 1;
 	ox = ox || 0;
 	oy = oy || 0;
+	this.ctx.textAlign=align;
+	
+	var words = t.split(' ');
+    var line = '';
+	
+	for(var i = 0; i < words.length; i++) {
+      var testLine = line + words[i] + ' ';
+      var metrics = this.ctx.measureText(testLine);
+      var testWidth = metrics.width;
+      if (testWidth > limit && i > 0) {
+      	this.ctx.save();
+		this.ctx.translate(x,y);
+		this.ctx.scale(sx,sy);
+		this.ctx.rotate(r);
+      	this.ctx.fillText(line, -ox,-oy);
+		this.ctx.restore();
 
-	this.ctx.save();
+        line = words[i] + ' ';
+        y += this.fontSize+10*sy;
+      }
+      else {
+        line = testLine;
+      }
+    }
+
+ 	this.ctx.save();
 	this.ctx.translate(x,y);
 	this.ctx.scale(sx,sy);
 	this.ctx.rotate(r);
-	if (img.loaded) {
-		this.ctx.drawImage(img,-ox,-oy);
-	}
+  	this.ctx.fillText(line, -ox,-oy);
 	this.ctx.restore();
-
+	this.ctx.textAlign="left";
 }
 
-love.graphics.draw = function (img,quad,x,y,r,sx,sy,ox,oy) {
-	if (typeof(quad) != "array") {
-		love.graphics._draw(img,quad,x,y,r,sx,sy,ox);
-		return;
+love.graphics._draw = function (img,x,y,r,sx,sy,ox,oy,kx,ky,quad) {
+	if (x == null) {
+		x = 0;
 	}
-	sx = sx || 1;
-	sy = sy || 1;
-	ox = ox || 0;
-	oy = oy || 0;
+	if (y == null) {
+		y = 0;
+	}
+	if (r == null) {
+		r = 0;
+	}
+	if (sx == null) {
+		sx = 1;
+	}
+	if (sy == null) {
+		sy = sx;
+	}
+	if (ox == null) {
+		ox = 0;
+	}
+	if (oy == null) {
+		oy = 0;
+	}
+	if (kx == null) {
+		kx = 0;
+	}
+	if (ky == null) {
+		ky = 0;
+	}
 
+	if (img.filter != "default") {
+		this.ctx.imageSmoothingEnabled = img.filter == "linear";
+	}
+	else {
+		this.ctx.imageSmoothingEnabled = this.defaultFilter;
+	}
 	this.ctx.save();
+	this.ctx.transform(1,ky,kx,1,0,0);
 	this.ctx.translate(x,y);
 	this.ctx.scale(sx,sy);
 	this.ctx.rotate(r);
-	if (img.loaded) {
-		this.ctx.drawImage(img,quad.sx,quad.sy,quad.sw,quad.sh,-ox,-oy,quad.sw,quad.sh);
+	if (quad) {
+		this.ctx.drawImage(love.graphics.images[img.url],quad.viewport.sx,quad.viewport.sy,quad.viewport.sw,quad.viewport.sh,-ox,-oy,quad.viewport.sw,quad.viewport.sh);
+	}
+	else{
+		this.ctx.drawImage(love.graphics.images[img.url],-ox,-oy);
 	}
 	this.ctx.restore();
+	this.ctx.imageSmoothingEnabled = this.defaultFilter == "linear";
+}
+
+love.graphics.draw = function (img,quad,x,y,r,sx,sy,ox,oy,kx,ky) {
+	if (typeof(quad) != "object") {
+		love.graphics._draw(img,quad,x,y,r,sx,sy,ox,oy,kx,ky);
+	}
+	else{
+		love.graphics._draw(img,x,y,r,sx,sy,ox,oy,kx,ky,quad);
+	}
 }
 
 
 
 //New functions
-
 love.graphics.newImage = function (url) {
-	var img = new Image();
-	img.src = url;
-	img.onload = function(){
-		this.loaded = true;
+	var img;
+	img = {};
+	img.url = url;
+	img.filter = "default";
+	img.wrap = "clamp"
+
+	img.typeOf = function (type) {
+		return type == "Object" || type == "Drawable" || type == "Texture" || type == "Image";
+	}
+
+	img.type = function () {
+		return "Image";
+	}
+
+	img.getFilter = function () {
+		return (this.filter=="default") ? love.graphics.defaultFilter : this.filter;
+	}
+	
+	img.getDimensions = function () {
+		return [this.width,this.height];
+	}
+
+	img.getWidth = function () {
+		return this.width;
+	}
+
+	img.getHeight = function () {
+		return this.height;
+	}
+
+	img.getWrap = function () {
+		return this.wrap;
+	}
+
+	img.getData = function () {
+
+	}
+
+	img.setFilter = function (filter) {
+		switch (filter) {
+			case "nearest":
+			    this.filter = "nearest";
+			    break;
+			case "linear":
+			    this.filter = "linear";
+			    break;
+			case null:
+				this.filter = "default";
+				break;
+			default:
+				throw("Invalid filter mode: " + filter)
+				break;
+		}
+	}
+
+	img.setWrap = function (wrap) {
+		switch (wrap) {
+			case "clamp":
+			    this.wrap = "clamp";
+			    break;
+			case "repeat":
+			    this.wrap = "repeat";
+			    break;
+			case null:
+				this.wrap = "clamp";
+				break;
+			default:
+				throw("Invalid filter mode: " + filter)
+				break;
+		}
 	}
 
 	return img;
 }
 
+
 love.graphics.newQuad = function (x,y,w,h,sw,sh,adw,adh) {
-	return {sx:x,sy:y,sw:w,sh:h};
+	var quad = {};
+	quad.viewport = {sx:x,sy:y,sw:w,sh:h}
+
+	quad.typeOf = function (type) {
+		return type == "Object" || type == "Quad";
+	}
+
+	quad.type = function () {
+		return "Quad";
+	}
+
+	quad.getViewport = function () {
+		return [this.viewport.sx,this.viewport.sy,this.viewport.sw,this.viewport.sh];
+	}
+
+	quad.setViewport = function (x,y,w,h) {
+		this.viewport = {sx:x,sy:y,sw:w,sh:h};
+	}
+	return quad;
 }
 
-love.graphics.newFont = function (font,size) {
-	return 'normal ' + size + 'px ' + font;
-}
 
+love.graphics.newFont = function (fnt,size) {
+	font = {};
+	font.size = size;
+	font.name = fnt;
+	font.filter = "default";
+
+	font.typeOf = function (type) {
+		return type == "Object" || type == "Font";
+	}
+
+	font.type = function () {
+		return "Font";
+	}
+
+	font.getFilter = function () {
+		return (this.filter=="default") ? love.graphics.defaultFilter : this.filter;
+	}
+
+	font.setFilter = function (filter) {
+		switch (filter) {
+			case "nearest":
+			    this.filter = "nearest";
+			    break;
+			case "linear":
+			    this.filter = "linear";
+			    break;
+			case null:
+				this.filter = "default";
+				break;
+			default:
+				throw("Invalid filter mode: " + filter)
+				break;
+		}
+	}
+
+	return font;
+}
 
 
 //Set functions
 
-love.graphics.setDefaultFilter = function (a) {
-	this.ctx.imageSmoothingEnabled = a == "linear";
+love.graphics.setDefaultFilter = function (filter) {
+	switch (filter) {
+	case "nearest":
+	    this.defaultFilter = "nearest";
+	    break;
+	case "linear":
+	    this.defaultFilter = "linear";
+	    break;
+	default:
+		throw("Invalid filter mode: " + filter)
+		break;
+	}
+	this.ctx.imageSmoothingEnabled = this.defaultFilter == "linear";
 }
 
+
 love.graphics.setColor = function (r,g,b,a) {
-	this.color.r = r.toString(16);
-	this.color.g = g.toString(16);
-	this.color.b = b.toString(16);
-	if (this.color.r.length == 1){
-		this.color.r = this.color.r + '0';
+	//TODO: Accept arrays
+	if (typeof(r)=="object") {
+		this.color.r = r[0] || this.color.r;
+		this.color.g = g[0] || this.color.g;
+		this.color.b = b[0] || this.color.b;
+		this.color.a = a[0] || this.color.a;
 	}
-	if (this.color.g.length == 1){
-		this.color.g = this.color.g + '0';
+	else {
+		this.color.r = r;
+		this.color.g = g;
+		this.color.b = b;
+		this.color.a = a;
 	}
-	if (this.color.b.length == 1){
-		this.color.b = this.color.b + '0';
-	}
-	this.ctx.fillStyle='#' + this.color.r + this.color.g + this.color.b;
-	this.ctx.strokeStyle='#' + this.color.r + this.color.g + this.color.b;
+	
+	
+	this.ctx.fillStyle = this.rgb(r,g,b);
+	this.ctx.strokeStyle = this.rgb(r,g,b);
 	love.graphics.ctx.globalAlpha = a/255;
 }
 
@@ -181,8 +409,6 @@ love.graphics.setBackgroundColor = function (r,g,b) {
 	if (this.backgroundColor.b.length == 1){
 		this.backgroundColor.b = this.backgroundColor.b + '0';
 	}
-	this.ctx.fillStyle='#' + this.backgroundColor.r + this.backgroundColor.g + this.backgroundColor.b;
-	this.ctx.strokeStyle='#' + this.backgroundColor.r + this.backgroundColor.g + this.backgroundColor.b;
 }
 
 love.graphics.setLineWidth = function (s) {
@@ -193,8 +419,9 @@ love.graphics.setPointSize = function (s) {
 	this.pointSize = s;
 }
 
-love.graphics.setFont = function (font) {
-	this.ctx.font = font;
+love.graphics.setFont = function (fnt) {
+	this.font = fnt;
+	this.ctx.font = this.font.size + "pt " + this.font.name
 }
 
 love.graphics.setBlendMode = function (mode) {
@@ -213,7 +440,7 @@ love.graphics.getDefaultFilter = function () {
 }
 
 love.graphics.getColor = function () {
-	return [parseInt(this.color.r,16),parseInt(this.color.g,16),parseInt(this.color.b,16)];
+	return [this.color.r,this.color,g,this.color.b,this.color.a];
 }
 
 love.graphics.getBackgroundColor = function () {
@@ -229,12 +456,16 @@ love.graphics.getPointSize = function () {
 }
 
 love.graphics.getFont = function () {
-	return this.ctx.font;
+	return this.font;
 }
 
 
 
 //Coordinate System
+love.graphics.origin = function () {
+	this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
 love.graphics.translate = function (x,y) {
 	this.ctx.translate(x,y);
 }
@@ -278,4 +509,9 @@ love.graphics.clearScreen = function () {
 
 love.graphics.background = function () {
 	this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+}
+
+love.graphics.rgb = function (r,g,b) {
+	var x = ((r << 16) | (g << 8) | b).toString(16);
+	return "#000000".substring(0, 7 - x.length) + x;
 }
